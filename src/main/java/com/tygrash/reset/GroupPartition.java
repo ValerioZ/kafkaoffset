bf7599c152c5b9c39246436068ea6d0e7bb91f6b
v.carlomusto <v.carlomusto@idscorporation.com>
2018-01-24 16:45:53 +0100

    Init repo

---
+++ src/main/java/com/tygrash/reset/GroupPartition.java
@@ -0,0 +1,64 @@
+package com.tygrash.reset;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+@Getter
+@AllArgsConstructor
+public class GroupPartition {
+    private final Logger logger = LoggerFactory.getLogger(GroupPartition.class.getName());
+
+    private String consumerGroup;
+    private String kafkaTopic;
+    private Integer partitionNumber;
+    private Long newOffset;
+
+//    public GroupPartition(String p_consumerGroup, String p_kafkaTopic, int p_partition, Long p_offset) {
+//        this.consumerGroup = p_consumerGroup;
+//        this.kafkaTopic = p_kafkaTopic;
+//        this.partitionNumber = p_partition;
+//        this.newOffset = p_offset;
+//
+//    }
+
+    public void doReset(KafkaConsumer consumer) {
+        List<PartitionInfo> topicPartitionsInKafka = consumer.partitionsFor(kafkaTopic);
+
+        if (isPartitionPresent(topicPartitionsInKafka, partitionNumber)) {
+            Map<TopicPartition, OffsetAndMetadata> offsets = createOffsetsAndMetadata(partitionNumber, kafkaTopic, newOffset);
+            consumer.commitSync(offsets);
+
+            logger.info("Successfully reset the newOffset to {} for topic {} partition {}", newOffset, kafkaTopic, partitionNumber);
+            return;
+        }
+
+        logger.error("GroupPartition {} for topic {} does not exist", partitionNumber, kafkaTopic);
+    }
+
+    private Map<TopicPartition, OffsetAndMetadata> createOffsetsAndMetadata(Integer partition, String kafkaTopic, Long offset) { // SUPPRESS CHECKSTYLE
+        Map<TopicPartition, OffsetAndMetadata> topicPartitionAndOffset = new HashMap<>();
+
+        TopicPartition topicPartition = new TopicPartition(kafkaTopic, partition);
+        OffsetAndMetadata offsetAndMetadata = new OffsetAndMetadata(offset);
+        topicPartitionAndOffset.put(topicPartition, offsetAndMetadata);
+
+        return topicPartitionAndOffset;
+    }
+
+    private boolean isPartitionPresent(List<PartitionInfo> topicPartitionsInKafka, Integer partitionNumberForReset) {
+        for (PartitionInfo partitionInfo : topicPartitionsInKafka) {
+            if (partitionInfo.partition() == partitionNumberForReset) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
